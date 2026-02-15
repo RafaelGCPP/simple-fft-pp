01c8b43af0009e026a07ae049b84ac68f1de5462
Rafael Pinto <rafaelgcpp@gmail.com>
2026-02-15 19:42:56 -0300

    Adding predefined types.

--- include/simple_fft.h
+++ include/simple_fft.h
@@ -2,25 +2,45 @@
 
 #include "fixed_point.h"
 #include "fft_core.h"
+#include "rfft.h"
 
-namespace sfft {
+namespace sfft
+{
 
-// Shortcuts for Fixed point Q23 FFTs with different sizes
-using FixedFFT1024Q23 = FFT<Q23Complex, Q31Complex,1024>;
-using FixedFFT512Q23 = FFT<Q23Complex, Q31Complex,512>;
-using FixedFFT256Q23 = FFT<Q23Complex, Q31Complex,256>;
-using FixedFFT128Q23 = FFT<Q23Complex, Q31Complex,128>;
-using FixedFFT64Q23 = FFT<Q23Complex, Q31Complex,64>;
-using FixedFFT32Q23 = FFT<Q23Complex, Q31Complex,32>;
-using FixedFFT16Q23 = FFT<Q23Complex, Q31Complex,16>;
+    // Shortcuts for Fixed point Q23 FFTs with different sizes
+    using FixedFFT1024Q23 = FFT<Q23Complex, Q31Complex, 1024>;
+    using FixedFFT512Q23 = FFT<Q23Complex, Q31Complex, 512>;
+    using FixedFFT256Q23 = FFT<Q23Complex, Q31Complex, 256>;
+    using FixedFFT128Q23 = FFT<Q23Complex, Q31Complex, 128>;
+    using FixedFFT64Q23 = FFT<Q23Complex, Q31Complex, 64>;
+    using FixedFFT32Q23 = FFT<Q23Complex, Q31Complex, 32>;
+    using FixedFFT16Q23 = FFT<Q23Complex, Q31Complex, 16>;
 
-//Shortcuts for Fixed point Q15 FFTs with different sizes
-using FixedFFT1024Q15 = FFT<Q15Complex, Q31Complex,1024>;
-using FixedFFT512Q15 = FFT<Q15Complex, Q31Complex,512>;
-using FixedFFT256Q15 = FFT<Q15Complex, Q31Complex,256>;
-using FixedFFT128Q15 = FFT<Q15Complex, Q31Complex,128>;
-using FixedFFT64Q15 = FFT<Q15Complex, Q31Complex,64>;
-using FixedFFT32Q15 = FFT<Q15Complex, Q31Complex,32>;
-using FixedFFT16Q15 = FFT<Q15Complex, Q31Complex,16>;
+    // Shortcuts for Fixed point Q15 FFTs with different sizes
+    using FixedFFT1024Q15 = FFT<Q15Complex, Q31Complex, 1024>;
+    using FixedFFT512Q15 = FFT<Q15Complex, Q31Complex, 512>;
+    using FixedFFT256Q15 = FFT<Q15Complex, Q31Complex, 256>;
+    using FixedFFT128Q15 = FFT<Q15Complex, Q31Complex, 128>;
+    using FixedFFT64Q15 = FFT<Q15Complex, Q31Complex, 64>;
+    using FixedFFT32Q15 = FFT<Q15Complex, Q31Complex, 32>;
+    using FixedFFT16Q15 = FFT<Q15Complex, Q31Complex, 16>;
+
+    // Shortcuts for Fixed point Q23 RFFTs with different sizes
+    using FixedRFFT1024Q23 = RFFT<Q23Complex, Q23Complex, Q31Complex, 1024>;
+    using FixedRFFT512Q23 = RFFT<Q23Complex, Q23Complex, Q31Complex, 512>;
+    using FixedRFFT256Q23 = RFFT<Q23Complex, Q23Complex, Q31Complex, 256>;
+    using FixedRFFT128Q23 = RFFT<Q23Complex, Q23Complex, Q31Complex, 128>;
+    using FixedRFFT64Q23 = RFFT<Q23Complex, Q23Complex, Q31Complex, 64>;
+    using FixedRFFT32Q23 = RFFT<Q23Complex, Q23Complex, Q31Complex, 32>;
+    using FixedRFFT16Q23 = RFFT<Q23Complex, Q23Complex, Q31Complex, 16>;
+
+    // Shortcuts for Fixed point Q15 RFFTs with different sizes
+    using FixedRFFT1024Q15 = RFFT<Q15Complex, Q15Complex, Q31Complex, 1024>;
+    using FixedRFFT512Q15 = RFFT<Q15Complex, Q15Complex, Q31Complex, 512>;
+    using FixedRFFT256Q15 = RFFT<Q15Complex, Q15Complex, Q31Complex, 256>;
+    using FixedRFFT128Q15 = RFFT<Q15Complex, Q15Complex, Q31Complex, 128>;
+    using FixedRFFT64Q15 = RFFT<Q15Complex, Q15Complex, Q31Complex, 64>;
+    using FixedRFFT32Q15 = RFFT<Q15Complex, Q15Complex, Q31Complex, 32>;
+    using FixedRFFT16Q15 = RFFT<Q15Complex, Q15Complex, Q31Complex, 16>;
 
 } // namespace sfft
--- include/simple_fft_complex.h
+++ include/simple_fft_complex.h
@@ -2,11 +2,13 @@
 
 #include <complex>
 #include "fft_core.h"
+#include "rfft.h"
 
 namespace sfft
 {
 
     using Complex = std::complex<float>;
+    using ComplexDouble = std::complex<double>;
 
     // Shortcuts for Complex FFTs with different sizes
     using ComplexFFT1024 = FFT<Complex, Complex, 1024>;
@@ -16,5 +18,32 @@ namespace sfft
     using ComplexFFT64 = FFT<Complex, Complex, 64>;
     using ComplexFFT32 = FFT<Complex, Complex, 32>;
     using ComplexFFT16 = FFT<Complex, Complex, 16>;
+
+    // Shortcuts for Complex RFFTs with different sizes
+    using ComplexRFFT1024 = RFFT<Complex, Complex, Complex, 1024>;
+    using ComplexRFFT512 = RFFT<Complex, Complex, Complex, 512>;
+    using ComplexRFFT256 = RFFT<Complex, Complex, Complex, 256>;
+    using ComplexRFFT128 = RFFT<Complex, Complex, Complex, 128>;
+    using ComplexRFFT64 = RFFT<Complex, Complex, Complex, 64>;
+    using ComplexRFFT32 = RFFT<Complex, Complex, Complex, 32>;
+    using ComplexRFFT16 = RFFT<Complex, Complex, Complex, 16>;
+
+    // Shortcuts for ComplexDouble FFTs with different sizes
+    using ComplexDoubleFFT1024 = FFT<ComplexDouble, ComplexDouble, 1024>;
+    using ComplexDoubleFFT512 = FFT<ComplexDouble, ComplexDouble, 512>;
+    using ComplexDoubleFFT256 = FFT<ComplexDouble, ComplexDouble, 256>;
+    using ComplexDoubleFFT128 = FFT<ComplexDouble, ComplexDouble, 128>;
+    using ComplexDoubleFFT64 = FFT<ComplexDouble, ComplexDouble, 64>;
+    using ComplexDoubleFFT32 = FFT<ComplexDouble, ComplexDouble, 32>;
+    using ComplexDoubleFFT16 = FFT<ComplexDouble, ComplexDouble, 16>;
+
+    // Shortcuts for ComplexDouble RFFTs with different sizes
+    using ComplexDoubleRFFT1024 = RFFT<ComplexDouble, ComplexDouble, ComplexDouble, 1024>;
+    using ComplexDoubleRFFT512 = RFFT<ComplexDouble, ComplexDouble, ComplexDouble, 512>;
+    using ComplexDoubleRFFT256 = RFFT<ComplexDouble, ComplexDouble, ComplexDouble, 256>;
+    using ComplexDoubleRFFT128 = RFFT<ComplexDouble, ComplexDouble, ComplexDouble, 128>;
+    using ComplexDoubleRFFT64 = RFFT<ComplexDouble, ComplexDouble, ComplexDouble, 64>;
+    using ComplexDoubleRFFT32 = RFFT<ComplexDouble, ComplexDouble, ComplexDouble, 32>;
+    using ComplexDoubleRFFT16 = RFFT<ComplexDouble, ComplexDouble, ComplexDouble, 16>;
 
 } // namespace sfft
